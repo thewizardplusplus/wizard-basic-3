cf8981fada87d21faecb2ac21fef648d2a8c77e6
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-01-22 19:41:50 +0300

    Add indenting of AST.

--- source/thewizardplusplus/wizard-basic-3/main.cpp
+++ source/thewizardplusplus/wizard-basic-3/main.cpp
@@ -3,10 +3,15 @@
 #include "parse.h"
 #include "translate.h"
 #include <iostream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 #include <boost/format.hpp>
 
 using namespace thewizardplusplus::wizard_basic_3;
 using namespace boost;
+using namespace boost::property_tree;
+
+const auto XML_INDENT_SIZE = 4;
 
 template<FinalStage final_stage, typename ResultType>
 void ProcessResult(
@@ -29,7 +34,25 @@ int main(int number_of_arguments, char* arguments[]) try {
 	ProcessResult<FinalStage::CODE>(command_line_arguments, code);
 
 	const auto ast = Parse(code);
-	ProcessResult<FinalStage::AST>(command_line_arguments, ast);
+	ProcessResult<FinalStage::AST>(
+		command_line_arguments,
+		([&] () -> std::string {
+			std::stringstream source;
+			source << ast;
+
+			ptree property_tree;
+			read_xml(source, property_tree);
+
+			std::ostringstream out;
+			write_xml(
+				out,
+				property_tree,
+				xml_writer_make_settings<std::string>(' ', XML_INDENT_SIZE)
+			);
+
+			return out.str();
+		})()
+	);
 
 	const auto ir = Translate(ast);
 	ProcessResult<FinalStage::IR>(command_line_arguments, ir);
