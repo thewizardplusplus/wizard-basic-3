365d7eea222ae5695b06a6491c3b5ed5f3e40887
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-01-26 10:32:29 +0300

    Add types for runtime.

--- wizard-basic-runtime/main.c
+++ wizard-basic-runtime/main.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 
+typedef enum ValueType {
+	NULL,
+	NUMBER,
+	ARRAY,
+	STRUCTURE
+} ValueType;
+
+typedef Number double;
+
+struct Value;
+typedef Array *struct Value;
+
+typedef struct ArrayData {
+	size_t size;
+	Array array;
+} ArrayData;
+
+typedef struct StructureData {
+	char* name;
+	void* structure;
+} StructureData;
+
+typedef union ValueStorage {
+	Number number;
+	ArrayData array;
+	StructureData structure;
+} ValueStorage;
+
+typedef struct Value {
+	ValueType type;
+	ValueStorage storage;
+} Value, *ValuePointer;
+
 int main(void) {
 	puts("Test.");
 }
