1e994d02da5055189133f5627b48e91e26b151fe
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-01-29 18:02:17 +0300

    Add array definition translation.

--- wizard-basic-interpreter/source/thewizardplusplus/wizard-basic-3/translate.cpp
+++ wizard-basic-interpreter/source/thewizardplusplus/wizard-basic-3/translate.cpp
@@ -1,8 +1,11 @@
 #include "translate.h"
+#include <algorithm>
 #include <boost/format.hpp>
+#include <boost/algorithm/string/join.hpp>
 
 using namespace thewizardplusplus::wizard_parser::node;
 using namespace boost;
+using namespace boost::algorithm;
 
 namespace thewizardplusplus {
 namespace wizard_basic_3 {
@@ -19,6 +22,18 @@ static auto TranslateExpression(const Node& ast) -> std::string {
 	} else if (ast.name == "string_definition") {
 		const auto string = ast.value;
 		return (format(R"(CreateArrayFromString("%s"))") % string).str();
+	} else if (ast.name == "array_definition") {
+		std::vector<std::string> items;
+		std::transform(
+			ast.children.begin(),
+			ast.children.end(),
+			std::back_inserter(items),
+			TranslateExpression
+		);
+
+		return (format("CreateArrayFromList(%d,%s)")
+			% items.size()
+			% join(items, ",")).str();
 	} else {
 		throw std::runtime_error("unknown expression");
 	}
