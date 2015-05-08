d14a75efd620bed7735f3b834fcb7c7d55386bdf
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-05-08 13:51:55 +0300

    Fixes #31: add grammar rule names.

---
+++ do-interpreter/source/thewizardplusplus/language_do/GrammarRule.cpp
@@ -0,0 +1,44 @@
+#include "GrammarRule.h"
+#include <boost/spirit/home/classic/core.hpp>
+
+using namespace thewizardplusplus::language_do;
+using namespace boost::spirit::classic;
+
+static auto ToId(const GrammarRule& rule) -> parser_id {
+	return parser_id(static_cast<size_t>(rule));
+}
+
+const GrammarRuleMap GRAMMAR_RULE_NAMES = {
+		{ToId(GrammarRule::ROOT), "ROOT"},
+		{ToId(GrammarRule::PROGRAM), "PROGRAM"},
+		{ToId(GrammarRule::LINE), "LINE"},
+
+		{ToId(GrammarRule::STATEMENT), "STATEMENT"},
+		{
+			ToId(GrammarRule::STATEMENT_ARRAY_DEFINITION),
+			"STATEMENT_ARRAY_DEFINITION"
+		},
+		{ToId(GrammarRule::STATEMENT_ASSIGN), "STATEMENT_ASSIGN"},
+		{ToId(GrammarRule::STATEMENT_JUMP), "STATEMENT_JUMP"},
+		{ToId(GrammarRule::STATEMENT_CONDITION), "STATEMENT_CONDITION"},
+
+		{ToId(GrammarRule::EXPRESSION_ADDITION), "EXPRESSION_ADDITION"},
+		{
+			ToId(GrammarRule::EXPRESSION_MULTIPLICATION),
+			"EXPRESSION_MULTIPLICATION"
+		},
+		{ToId(GrammarRule::EXPRESSION_ATOM), "EXPRESSION_ATOM"},
+
+		{ToId(GrammarRule::COMMENT), "COMMENT"},
+		{ToId(GrammarRule::IDENTIFIER), "IDENTIFIER"},
+		{ToId(GrammarRule::ARRAY_ACCESS), "ARRAY_ACCESS"},
+		{ToId(GrammarRule::FUNCTION_CALL), "FUNCTION_CALL"},
+
+		{ToId(GrammarRule::LINE_BREAK), "LINE_BREAK"},
+		{
+			ToId(GrammarRule::CONSTANT_POSITIVE_INTEGER),
+			"CONSTANT_POSITIVE_INTEGER"
+		},
+		{ToId(GrammarRule::CONSTANT_REAL), "CONSTANT_REAL"},
+		{ToId(GrammarRule::CONSTANT_STRING), "CONSTANT_STRING"},
+	};
---
+++ do-interpreter/source/thewizardplusplus/language_do/GrammarRule.h
@@ -0,0 +1,41 @@
+#ifndef DO_GRAMMAR_RULE_HEADER
+#define DO_GRAMMAR_RULE_HEADER
+
+#include <boost/spirit/home/classic/core.hpp>
+#include <map>
+
+namespace thewizardplusplus {
+namespace language_do {
+
+enum class GrammarRule : uint8_t {
+	ROOT,
+	PROGRAM,
+	LINE,
+
+	STATEMENT,
+	STATEMENT_ARRAY_DEFINITION,
+	STATEMENT_ASSIGN,
+	STATEMENT_JUMP,
+	STATEMENT_CONDITION,
+
+	EXPRESSION_ADDITION,
+	EXPRESSION_MULTIPLICATION,
+	EXPRESSION_ATOM,
+
+	COMMENT,
+	IDENTIFIER,
+	ARRAY_ACCESS,
+	FUNCTION_CALL,
+
+	LINE_BREAK,
+	CONSTANT_POSITIVE_INTEGER,
+	CONSTANT_REAL,
+	CONSTANT_STRING
+};
+using GrammarRuleMap = std::map<boost::spirit::classic::parser_id, std::string>;
+
+extern const GrammarRuleMap GRAMMAR_RULE_NAMES;
+
+}
+}
+#endif
