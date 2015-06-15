71efa9e67cf252a1d49c5db3f43f513f0159c6d0
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-06-15 14:22:25 +0300

    Correct examples.

--- docs/examples/exit_code_test/exit_code_test.sh
+++ docs/examples/exit_code_test/exit_code_test.sh
@@ -2,5 +2,5 @@
 
 readonly base_path=`dirname $0`
 readonly script_name=`basename $0 | sed 's/\.[^\.]\+$//'`
-$base_path/../../../build/doi $base_path/$script_name.do
+$base_path/../../../build/wb3 $base_path/$script_name.wb3
 printf "Exit code: %d.\n" $?
